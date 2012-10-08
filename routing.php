64af4e77107b4a6af207e765f2f98d7dd7d44e17
Brian Scaturro <scaturrob@gmail.com>
2012-10-08 18:52:07 -0400

    rename server handling file

--- routing.php
+++ routing.php
@@ -7,11 +7,12 @@
  * Otherwise serve up the JavaScript app
  */
 $uri = $_SERVER['REQUEST_URI'];
-$words = '/^\/words(\/[\w]+)?/';
+$words = '/^\/words(\/[\w]+)/';
+echo $uri;
 if(file_exists(__DIR__ . DIRECTORY_SEPARATOR . $uri)) {
     return false;
 else if (preg_match($words, $uri)) {
-    require_once __DIR__ . DIRECTORY_SEPARATOR . 'words' . DIRECTORY_SEPARATOR . 'main.php';
+    require_once __DIR__ . DIRECTORY_SEPARATOR . 'words' . DIRECTORY_SEPARATOR . 'index.php';
 } else {
     require_once 'index.html';
 }
